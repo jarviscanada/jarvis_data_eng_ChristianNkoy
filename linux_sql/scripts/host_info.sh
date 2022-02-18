76c4f911ef902a590a370213ae8c2638e0258e09
Christian Nkoy <nkoycm@gmail.com>
2022-02-18 21:45:11 +0000

    merged feature/sql_queries

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> feature/sql_queries
 #!/bin/bash
 
 #Store CLI parameters as variables
@@ -23,7 +28,15 @@ mem_info=$(cat /proc/meminfo)
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print$2}' | xargs)
 cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print$3, $4, $5, $6, $7, $8, $9, $10}' | xargs)
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+
+=======
+echo "Model name is $cpu_model"
+>>>>>>> 4a16c56cc10d888075ba56b1183941e603a7e16c
+>>>>>>> feature/sql_queries
 cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print$3}' | xargs)
 l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print$3}' | xargs)
 total_mem=$(echo "$mem_info" | egrep "^MemTotal:" | awk '{print$2}' | xargs)
@@ -40,4 +53,13 @@ export PGPASSWORD=$psql_password
 
 #Execute insert statement
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+<<<<<<< HEAD
 exit $?
+=======
+<<<<<<< HEAD
+exit $?
+=======
+exit $?
+>>>>>>> 4a16c56cc10d888075ba56b1183941e603a7e16c
+>>>>>>> Stashed changes
+>>>>>>> feature/sql_queries
